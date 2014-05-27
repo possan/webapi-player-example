dd458fbe9571dfad16883ef246d0a4c15499d754
Per-Olov Jernberg <po@spotify.com>
2014-05-27 19:00:58 +0200

    Playlists listed

--- controllers/player.js
+++ controllers/player.js
@@ -2,9 +2,29 @@
 
 	var module = angular.module('PlayerApp');
 
-	module.controller('PlayerController', function($scope, Auth, PlayQueue) {
+	module.controller('PlayerController', function($scope, Auth, API, PlayQueue) {
 		$scope.view = 'welcome';
 		$scope.username = Auth.getUsername();
+		$scope.playlists = [];
+
+		function updatePlaylists() {
+			if ($scope.username != '') {
+				API.getPlaylists().then(function(list) {
+					$scope.playlists = list.items.map(function(pl) {
+						return {
+							id: pl.id,
+							name: pl.name,
+							uri: pl.uri,
+							collaborative: pl.collaborative,
+							'public': pl['public']
+						};
+					});
+					$scope.$apply();
+				});
+			}
+		}
+
+		updatePlaylists();
 
 		$scope.logout = function() {
 			// do login!
@@ -20,8 +40,21 @@
 			PlayQueue.enqueue(trackuri);
 		}
 
+		$scope.showhome = function() {
+			console.log('load home view');
+		}
+
+		$scope.showplayqueue = function() {
+			console.log('load playqueue view');
+		}
+
+		$scope.showplaylist = function(playlisturi) {
+			console.log('load playlist view', playlisturi);
+		}
+
 		$scope.$on('login', function() {
 			$scope.username = Auth.getUsername();
+			updatePlaylists();
 		});
 
 		$scope.$on('playqueuechanged', function() {
--- services/api.js
+++ services/api.js
@@ -3,11 +3,8 @@
 	var module = angular.module('PlayerApp');
 
 	module.factory('API', function(Auth, $q, $http) {
-
-
-
-
 		return {
+
 			getMyUsername: function() {
 				var ret = $q.defer();
 				$http.get('https://api.spotify.com/v1/me', {
@@ -15,11 +12,25 @@
 						'Authorization': 'Bearer ' + Auth.getAccessToken()
 					}
 				}).success(function(r) {
-					console.log('got',r);
+					console.log('got userinfo',r);
 					ret.resolve(r.id);
 				});
 				return ret.promise;
 			},
+
+			getPlaylists: function() {
+				var ret = $q.defer();
+				$http.get('https://api.spotify.com/v1/users/' + Auth.getUsername() + '/playlists', {
+					headers: {
+						'Authorization': 'Bearer ' + Auth.getAccessToken()
+					}
+				}).success(function(r) {
+					console.log('got playlists',r);
+					ret.resolve(r);
+				});
+				return ret.promise;
+			}
+
 		}
 	});
 
