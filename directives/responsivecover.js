4f494ebe653836123e70da3fba3e18ebc0fe24d6
José M. Pérez <jmperez1985@gmail.com>
2014-07-23 09:07:05 +0200

    Add directive to encapsulate image rendering to right size

---
+++ directives/responsivecover.js
@@ -0,0 +1,61 @@
+(function() {
+
+	var module = angular.module('PlayerApp');
+
+	module.directive('responsiveCover', function() {
+		return {
+			restrict: 'E',
+			scope: {
+				imagesData: '=ngModel'
+			},
+			replace: true,
+			compile: function (tElem, tAttrs) {
+				var linkFunction = function($scope, element, attributes) {
+				  var elem = element[0];
+					
+					var interval = null;
+					$scope.$watch('imagesData', function() {
+						if (interval) {
+						  clearInterval(interval);
+						  interval = null;
+						}
+
+						if ($scope.imagesData) {
+						  try {
+							var images = $scope.imagesData;
+
+							// todo: once we have loaded a large one maybe it doesn't make sense to try to load
+							// a smaller one, since that incurs in an extra request and the quality won't be
+							// better than the one provided by the larger image
+
+							var findRightImage = function() {
+							  var targetWidth = elem.offsetWidth * window.devicePixelRatio,
+								  targetHeight = elem.offsetHeight * window.devicePixelRatio;
+
+							  if (targetWidth === 0 || targetHeight === 0) {
+								return;
+							  }
+
+							  var cover = images[0].url;
+							  for (var i=1; i<images.length; i++) {
+								if (images[i].width >= targetWidth && images[i].height >= targetHeight) {
+								  cover = images[i].url;
+								}
+							  }
+
+							  elem.style.backgroundImage = 'url(' + cover + ')';
+							}.bind(this);
+							interval = setInterval(findRightImage, 200);
+							findRightImage();
+						  } catch (e) {
+							console.error(e);
+						  }
+						}
+					});
+				};
+				return linkFunction;
+			}
+		};
+	});
+
+})();
